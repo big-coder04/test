07301fa8e454429dffc00214112a93782d9dbea1
big-coder04 <saipraveen1969@gmail.com>
2022-09-23 13:01:08 +0530

    own coding

--- main.java
+++ main.java
@@ -1,6 +1,5 @@
-public class Main {
-    public static void main (Sting[] args ) {
-        System.output.println("Hello world i am in a terminal editor")
-        System.output.println("i should complete my dsa before my 2 nd semester");
-    }
-}
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}  
