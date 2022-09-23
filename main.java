254ebfc5b6a53f6e278c1124d394bbe2763d2e87
big-coder04 <114086532+big-coder04@users.noreply.github.com>
2022-09-23 12:16:48 +0530

    new one

--- main.java
+++ main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main (Sting[] args ) {
-        System.output.println("Hello world i am in a terminal editor");
+        System.output.println("Hello world i am in a terminal editor")
+        System.output.println("i should complete my dsa before my 2 nd semester");
     }
 }
